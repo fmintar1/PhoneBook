70bec581a21eedd35e71f8a00568bdd30f5136e2
Freddy <freddy@Zipcoders-MacBook-Pro-7.local>
2022-11-08 17:37:40 -0500

    The true meaning of cohort

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,11 +1,6 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.ArrayList;
-//import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by leon on 1/23/18.
@@ -16,17 +11,19 @@ public class PhoneBook {
     private final Map<String, List<String>> phonebook;
 
     public PhoneBook(Map<String, List<String>> map) {
-        this.phonebook = null;
+        this.phonebook = map;
     }
 
     public PhoneBook() {
-        this(null);
+        this(new LinkedHashMap<String, List<String>>());
     }
 
     public void add(String name, String phoneNumber) {
+        phonebook.put(name, new ArrayList<String>());
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        phonebook.put(name, new ArrayList<String>(List.of(phoneNumbers)));
     }
 
     public void remove(String name) {
@@ -36,8 +33,12 @@ public class PhoneBook {
         return null;
     }
 
-    public List<String> lookup(String name) {
+    public Boolean hasEntry(String name, String phoneNumber) {
         return null;
+    }
+
+    public List<String> lookup(String name) {
+        return phonebook.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
@@ -45,10 +46,11 @@ public class PhoneBook {
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        Set<String> keys = getMap().keySet();
+        return new ArrayList<>(keys);
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return phonebook;
     }
 }
